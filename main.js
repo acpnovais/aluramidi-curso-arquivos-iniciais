9a24b094a9739021697e8edb82caa01ceb993e19
Ana Novais <91226340+acpnovais@users.noreply.github.com>
2022-03-14 18:16:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play(); 
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+/*
+let contador = 0;
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    contador++;
+}
+*/
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
